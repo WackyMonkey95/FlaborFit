033fb88d4a88541903468d4e7e31707ab9564483
DESKTOP-2N3KM0P\WackyMonkey <maikel-emmanuel@hotmail.nl>
2018-03-22 12:38:32 +0100

    added onClicklistener

--- app/src/main/java/com/example/maike/flaborfit/MainActivity.java
+++ app/src/main/java/com/example/maike/flaborfit/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.maike.flaborfit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String EXERCISE_WEIGHTS = "Weight Lifting";
+    public static final String EXERCISE_YOGA = "Yoga";
+    public static final String EXERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.weight).setOnClickListener(this);
+        findViewById(R.id.yoga).setOnClickListener(this);
+        findViewById(R.id.cardio).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.weight:
+                loadDetailActivity(EXERCISE_WEIGHTS);
+                break;
+            case R.id.yoga:
+                loadDetailActivity(EXERCISE_YOGA);
+                break;
+            case R.id.cardio:
+                loadDetailActivity(EXERCISE_CARDIO);
+                break;
+            default:
+                break;
+
+
+        }
+    }
+    private void loadDetailActivity(String exerciseTitle){
+
     }
 }
